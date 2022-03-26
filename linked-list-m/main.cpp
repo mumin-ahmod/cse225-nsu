59dfd778ecf945918d634a0659262419d46c2ac5
mumin-ahmod <mumin.ahmed.ledp@gmail.com>
2022-03-26 20:12:45 +0600

    formatting

--- linked-list-m/main.cpp
+++ linked-list-m/main.cpp
@@ -9,25 +9,19 @@ int main()
 
     LinkedList<int> l1;
 
-    //l1.addNodeAtFront(5);
-    // l1.addNodeAtFront(10);
-    // l1.addNodeAtFront(15);
-    // l1.addNodeAtFront(20);
+    l1.addNodeAtFront(5);
+    l1.addNodeAtFront(10);
+    l1.addNodeAtFront(15);
+    l1.addNodeAtFront(20);
 
     // l1.addNodeAtLast(2);
 
-   // l1.deleteFirstNode();
-   // l1.deleteLastNode();
+    // l1.deleteFirstNode();
+    // l1.deleteLastNode();
 
-   l1.deleteNode(5);
-
-
-
-
+    l1.deleteNode(5);
 
     l1.printNodes();
-
-    
 
     return 0;
 }
